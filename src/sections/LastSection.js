88e911b9d2327577828ac71675e9c537f9e95df0
Rakesh Pradhan <pradhanrakesh558@gmail.com>
2021-09-20 20:01:59 +0530

    fixed sizing

--- src/sections/LastSection.js
+++ src/sections/LastSection.js
@@ -6,11 +6,11 @@ const LastSection = () => {
       <div className="container">
         <div className="sm:flex sm:items-center">
           <div className="sm:w-1/2">
-            <h6 className="text-gray text-xl text-center mb-8">
+            <h6 className="text-gray text-xl sm:text-5xl text-center mb-8  leading-relaxed  mx-auto sm:mx-0 sm:text-left">
               Wanna see then rarest Potatoes?
             </h6>
           </div>
-          <div className="max-w-xs mx-auto sm:w-1/2">
+          <div className="max-w-xs md:max-w-lg mx-auto sm:w-1/2">
             <img src={Image2} alt="image2" />
           </div>
         </div>
