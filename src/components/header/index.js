e7d216ed6d7af4c0901f46b4de4a0e89792a8e85
Rakesh Pradhan <pradhanrakesh558@gmail.com>
2021-09-23 16:16:50 +0530

    remove static date

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,12 +1,13 @@
 import React from "react";
-
+import Timer from "../timer";
 const Index = () => {
   return (
     <header className="bg-primary">
       <div className="container p-4">
         <div className=" w-full max-w-xl mx-auto flex justify-between items-center flex-col sm:flex-row">
           <h1 className="text-md sm:text-xl mb-2 ">Mint Day</h1>
-          <p className=" text-xl sm:text-2xl">30 Sepetember 2021</p>
+          <Timer />
+          {/* <p className=" text-xl sm:text-2xl">30 Sepetember 2021</p> */}
         </div>
       </div>
     </header>
